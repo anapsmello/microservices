978eb36588c93ef5130108d477f7127f196fe9c6
Ana Mello <aninha.cadastros@gmail.com>
2025-06-01 12:21:54 -0300

    incluindo @Bean na function criada

--- section13-ana/accounts/src/main/java/com/eazybytes/accounts/functions/AccountsFunctions.java
+++ section13-ana/accounts/src/main/java/com/eazybytes/accounts/functions/AccountsFunctions.java
@@ -3,6 +3,7 @@ package com.eazybytes.accounts.functions;
 import com.eazybytes.accounts.service.IAccountsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
@@ -12,6 +13,7 @@ public class AccountsFunctions {
 
     private static final Logger log = LoggerFactory.getLogger(AccountsFunctions.class);
 
+    @Bean
     public Consumer<Long> updateCommunication(IAccountsService accountsService) {
         return accountNumber -> {
             log.info("Updating Communication status for the account number: " + accountNumber.toString());
